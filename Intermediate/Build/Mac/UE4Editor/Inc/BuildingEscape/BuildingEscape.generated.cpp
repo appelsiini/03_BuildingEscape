5068f14755d5037d52c0abde98793bcf8f1927cd
appelsiini <antti.rossi@finternet-group.com>
2017-06-27 14:50:56 +0300

    Reinstall everything, trying VSC since cant get Xcode code complete working..

---
+++ Intermediate/Build/Mac/UE4Editor/Inc/BuildingEscape/BuildingEscape.generated.cpp
@@ -0,0 +1,126 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "GeneratedCppIncludes.h"
+#include "BuildingEscape.generated.dep.h"
+PRAGMA_DISABLE_OPTIMIZATION
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCode1BuildingEscape() {}
+#if USE_COMPILED_IN_NATIVES
+// Cross Module References
+	ENGINE_API class UClass* Z_Construct_UClass_AGameModeBase();
+	ENGINE_API class UClass* Z_Construct_UClass_UActorComponent();
+
+	BUILDINGESCAPE_API class UClass* Z_Construct_UClass_ABuildingEscapeGameModeBase_NoRegister();
+	BUILDINGESCAPE_API class UClass* Z_Construct_UClass_ABuildingEscapeGameModeBase();
+	BUILDINGESCAPE_API class UClass* Z_Construct_UClass_UPositionReport_NoRegister();
+	BUILDINGESCAPE_API class UClass* Z_Construct_UClass_UPositionReport();
+	BUILDINGESCAPE_API class UPackage* Z_Construct_UPackage__Script_BuildingEscape();
+	void ABuildingEscapeGameModeBase::StaticRegisterNativesABuildingEscapeGameModeBase()
+	{
+	}
+	UClass* Z_Construct_UClass_ABuildingEscapeGameModeBase_NoRegister()
+	{
+		return ABuildingEscapeGameModeBase::StaticClass();
+	}
+	UClass* Z_Construct_UClass_ABuildingEscapeGameModeBase()
+	{
+		static UClass* OuterClass = NULL;
+		if (!OuterClass)
+		{
+			Z_Construct_UClass_AGameModeBase();
+			Z_Construct_UPackage__Script_BuildingEscape();
+			OuterClass = ABuildingEscapeGameModeBase::StaticClass();
+			if (!(OuterClass->ClassFlags & CLASS_Constructed))
+			{
+				UObjectForceRegistration(OuterClass);
+				OuterClass->ClassFlags |= 0x20900288;
+
+
+				static TCppClassTypeInfo<TCppClassTypeTraits<ABuildingEscapeGameModeBase> > StaticCppClassTypeInfo;
+				OuterClass->SetCppTypeInfo(&StaticCppClassTypeInfo);
+				OuterClass->StaticLink();
+#if WITH_METADATA
+				UMetaData* MetaData = OuterClass->GetOutermost()->GetMetaData();
+				MetaData->SetValue(OuterClass, TEXT("HideCategories"), TEXT("Info Rendering MovementReplication Replication Actor Input Movement Collision Rendering Utilities|Transformation"));
+				MetaData->SetValue(OuterClass, TEXT("IncludePath"), TEXT("BuildingEscapeGameModeBase.h"));
+				MetaData->SetValue(OuterClass, TEXT("ModuleRelativePath"), TEXT("BuildingEscapeGameModeBase.h"));
+				MetaData->SetValue(OuterClass, TEXT("ShowCategories"), TEXT("Input|MouseInput Input|TouchInput"));
+#endif
+			}
+		}
+		check(OuterClass->GetClass());
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(ABuildingEscapeGameModeBase, 4068982692);
+	static FCompiledInDefer Z_CompiledInDefer_UClass_ABuildingEscapeGameModeBase(Z_Construct_UClass_ABuildingEscapeGameModeBase, &ABuildingEscapeGameModeBase::StaticClass, TEXT("/Script/BuildingEscape"), TEXT("ABuildingEscapeGameModeBase"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(ABuildingEscapeGameModeBase);
+	void UPositionReport::StaticRegisterNativesUPositionReport()
+	{
+	}
+	UClass* Z_Construct_UClass_UPositionReport_NoRegister()
+	{
+		return UPositionReport::StaticClass();
+	}
+	UClass* Z_Construct_UClass_UPositionReport()
+	{
+		static UClass* OuterClass = NULL;
+		if (!OuterClass)
+		{
+			Z_Construct_UClass_UActorComponent();
+			Z_Construct_UPackage__Script_BuildingEscape();
+			OuterClass = UPositionReport::StaticClass();
+			if (!(OuterClass->ClassFlags & CLASS_Constructed))
+			{
+				UObjectForceRegistration(OuterClass);
+				OuterClass->ClassFlags |= 0x20B00080;
+
+
+				static TCppClassTypeInfo<TCppClassTypeTraits<UPositionReport> > StaticCppClassTypeInfo;
+				OuterClass->SetCppTypeInfo(&StaticCppClassTypeInfo);
+				OuterClass->StaticLink();
+#if WITH_METADATA
+				UMetaData* MetaData = OuterClass->GetOutermost()->GetMetaData();
+				MetaData->SetValue(OuterClass, TEXT("BlueprintSpawnableComponent"), TEXT(""));
+				MetaData->SetValue(OuterClass, TEXT("ClassGroupNames"), TEXT("Custom"));
+				MetaData->SetValue(OuterClass, TEXT("IncludePath"), TEXT("PositionReport.h"));
+				MetaData->SetValue(OuterClass, TEXT("ModuleRelativePath"), TEXT("PositionReport.h"));
+#endif
+			}
+		}
+		check(OuterClass->GetClass());
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(UPositionReport, 2750835968);
+	static FCompiledInDefer Z_CompiledInDefer_UClass_UPositionReport(Z_Construct_UClass_UPositionReport, &UPositionReport::StaticClass, TEXT("/Script/BuildingEscape"), TEXT("UPositionReport"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(UPositionReport);
+	UPackage* Z_Construct_UPackage__Script_BuildingEscape()
+	{
+		static UPackage* ReturnPackage = nullptr;
+		if (!ReturnPackage)
+		{
+			ReturnPackage = CastChecked<UPackage>(StaticFindObjectFast(UPackage::StaticClass(), nullptr, FName(TEXT("/Script/BuildingEscape")), false, false));
+			ReturnPackage->SetPackageFlags(PKG_CompiledIn | 0x00000000);
+			FGuid Guid;
+			Guid.A = 0x8A784089;
+			Guid.B = 0x3BE43A02;
+			Guid.C = 0x00000000;
+			Guid.D = 0x00000000;
+			ReturnPackage->SetGuid(Guid);
+
+		}
+		return ReturnPackage;
+	}
+#endif
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
+PRAGMA_ENABLE_OPTIMIZATION
---
+++ Intermediate/Build/Mac/UE4Editor/Inc/BuildingEscape/BuildingEscape.generated.dep.h
@@ -0,0 +1,13 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "GeneratedCppIncludes.h"
+#ifndef BUILDINGESCAPE_BuildingEscapeGameModeBase_generated_h
+	#include "BuildingEscapeGameModeBase.h"
+#endif
+#ifndef BUILDINGESCAPE_PositionReport_generated_h
+	#include "PositionReport.h"
+#endif
